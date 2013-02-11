f05fa8842002addf5361b80ad62570bafd8cc0bd
Kevin Wolf <kwolf72@gmail.com>
2013-02-10 18:18:15 -0600

    Somewhat working.

--- lib/amazon_wish_list.rb
+++ lib/amazon_wish_list.rb
@@ -1,21 +1,51 @@
 class AmazonWishList
   require 'uri'
-  require 'net/http'
+  require 'mechanize'
 
-  $search_url = URI.parse('http://www.amazon.com/gp/registry/search.html/ref=cm_wl_search_go?ie=UTF8&type=wishlist')
+  $search_url = 'http://www.amazon.com/gp/wishlist'
+  $root_url = 'http://amazon.com'
   $provider_name = 'Amazon'
-  $provider_version = '0.5'
+  $provider_version = '0.1'
 
-  $params = {'index' => 'us-xml-wishlist',
-             'registry-error-page' => 'templates/search/us-xml-wishlist-result.html',
-             'field-name' => '', 'x' => '0', 'y' => '0'}
+  $search_field_name = 'field-name'
+  $search_form_name = 'search-box'
 
-  $subst_field = 'index'
+  $subst_field = 'field-name'
 
-  def self.search field
-    $params[$subst_field] = field
-    page = Net::HTTP.post_form($search_url, $params)
+  def initialize
+    @agent = Mechanize.new()
   end
 
+  def search(field)
+    find_wish_lists field
+  end
 
+  #private
+
+  def find_wish_lists(field)
+    @agent.get($search_url)
+    form = @agent.page.form_with(name: $search_form_name)
+    form[$search_field_name] = field
+    if form.submit()
+      parse_lists
+    end
+  end
+
+  def parse_lists
+    lists = @agent.page.search('//div[starts-with(@id, "regListsList")]')
+    $wish_lists = Array.new
+
+    puts "Found #{lists.count} wish lists."
+
+    lists.each do |list|
+      wish_list = Hash.new()
+      wish_list[:name] = list.search('.regListUnsel').text
+      wish_list[:url] = $root_url + list.search('a')[0]['href']
+      wish_list[:items] = list.search('.regListCount').text
+
+      $wish_lists << wish_list
+    end
+
+    $wish_lists
+  end
 end
